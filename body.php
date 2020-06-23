cecc7428124e7d1e42dbc0ff8d694b443ff932a1
oohamaka <type_r_bathurst@me.com>
2020-06-24 06:37:13 +0900

    0624追記

---
+++ body.php
@@ -0,0 +1,21 @@
+<?php
+$id = $_POST['id'];
+$title = $_POST['title'];
+$body = $_POST['body'];
+echo $id;
+echo $title;
+echo $body;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ファイルの内容を表示</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
